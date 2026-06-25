92adaf75464a910b6273d61bb6f8a57d85ed9b36
amorancho <alvaromm22@gmail.com>
2026-06-25 11:00:39 +0200

    ajustes seo finales

--- site.config.mjs
+++ site.config.mjs
@@ -1,3 +1,5 @@
+const sharedLogoPath = "/images/new_logo.png";
+
 export const siteConfig = {
   name: "LumoraEvents",
   siteUrl: "https://lumoraevents.net",
@@ -5,6 +7,8 @@ export const siteConfig = {
   email: "info@lumoraevents.net",
   instagramUrl: "https://instagram.com/lumoraevents.app",
   instagramHandle: "@lumoraevents.app",
+  organizationLogo: sharedLogoPath,
+  brandLogo: sharedLogoPath,
   ogImage: "/images/social/og-lumoraevents.jpg",
   ogImageAlt: "LumoraEvents - Competition Technology Platform",
   themeColor: "#0a0a0a",
